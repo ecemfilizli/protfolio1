854ee1d5ae140abe4895223773dd57ca16068b3c
Ecem Filizli <ecmflzl@gmail.com>
2022-05-10 16:14:56 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+var Projedata = document.querySelector('.projects_img');
+
+var ProjeDetail = [
+    {resim:"assets/img/project1.jpg" , alt:""},
+    {resim:(document.getElementById("photos").src="assets/img/project1.jpg") ,alt:""},
+    {resim:"./image/abc.jpg",alt:""},
+    {resim:"./image/abc.jpg", alt:""},
+]
+
+document.getElementById('projecard').innerHTML = ProjeDetail.map(Proje => 
+    `<div>
+      
+      <img>resim: ${Proje.resim}</img>
+      <div>alt: ${Proje.alt}</div>
+    </div>
+    `
+).join('')
+
 /*=============== FILTERS TABS ===============*/
 
 
